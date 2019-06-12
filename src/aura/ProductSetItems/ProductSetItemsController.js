86c3c193fa87ffd276e68455d43e5e2de601e202
ushako21 <ushako21@ukr.net>
2019-06-12 12:09:01 +0300

    add button NewSet

--- src/aura/ProductSetItems/ProductSetItemsController.js
+++ src/aura/ProductSetItems/ProductSetItemsController.js
@@ -8,6 +8,17 @@
     },
 
     handleSelectProductSet : function (component, event, helper) {
-        helper.fireSelectProductSetEvent(component, event, helper)
+        helper.fireSelectProductSetEvent(component, event, helper);
+    },
+
+    closeModel : function (component, event, helper) {
+        helper.closeModel(component, event, helper);
+    },
+
+    saveNewSet : function (component, event, helper) {
+        helper.handleNewSetSave(component, event, helper);
+        helper.createRecord(component, event, helper);
+        helper.closeModel(component, event, helper);
+        helper.showToast(component, event, helper);
     }
 });
--- src/aura/ProductSetItems/ProductSetItemsHelper.js
+++ src/aura/ProductSetItems/ProductSetItemsHelper.js
@@ -19,11 +19,64 @@
     },
 
     fireSelectProductSetEvent : function (component, event, helper) {
-        var selectedPSEvent = component.getEvent("changeProductSet");
         var selected = component.find("selectProductSet").get("v.value");
-        selectedPSEvent.setParams({
-            "productSetId" : selected
+        if (selected === "New Set"){
+            helper.createRecord(component, event, helper);
+            // console.log("matched");
+            // component.set("v.isOpen", true);
+            // console.log(component.get("v.isOpen"));
+        }else {
+            var selectedPSEvent = component.getEvent("changeProductSet");
+            selectedPSEvent.setParams({
+                "productSetId": selected
+            });
+            selectedPSEvent.fire();
+        }
+    },
+
+    closeModel: function(component, event, helper) {
+        component.set("v.isOpen", false);
+    },
+
+    handleNewSetSave : function (component, event, helper) {
+
+        var action = component.get("c.createNewSet");
+        action.setParams({
+            name: component.get("v.newSetName")
         });
-        selectedPSEvent.fire();
+
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if(component.isValid() && state === "SUCCESS"){
+                console.log(component.get("v.newSetName") + " is created");
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        }));
+        $A.enqueueAction(action);
+    },
+
+    createRecord : function (component, event, helper) {
+        var createNewProductSet = $A.get("e.force:createRecord");
+        createNewProductSet.setParams({
+            "entityApiName": "Product_Set__c",
+            "defaultFieldValues": {
+                'Name' : component.get("v.newSetName"),
+                'Total_Price__c' : 0,
+                'Highest_Item_Price__c' : 0
+            }
+        });
+        createNewProductSet.fire();
+    },
+
+    showToast : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": component.get("v.newSetName"),
+            "message": "Set has been created successfully",
+            "type": "success"
+        });
+        toastEvent.fire();
     }
 });
